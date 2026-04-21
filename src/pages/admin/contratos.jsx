66dd87f71dc72970a83d8a4e0855d0cacc3ca01a
Jespindola15 <Jespindola@frsn.utn.edu.ar>
2026-04-21 18:12:50 -0300

    agregar form en contratos + correccion de boton guardar.

--- src/pages/admin/contratos.jsx
+++ src/pages/admin/contratos.jsx
@@ -1,9 +1,51 @@
 import "./admin.css";
+import { useState } from "react";
 
 export default function Contratos() {
+  const [showForm, setShowForm] = useState(false);
+
   return (
+  
     <main className="admin-page">
       <h1>Esta es la página Contratos</h1>
+
+       {/* Overlay oscuro */}
+      {showForm && (
+        <div className="modal-overlay" onClick={() => setShowForm(false)}></div>
+      )}
+
+      {/* Modal flotante */}
+      {showForm && (
+        <div className="modal-form">
+          <h2>Nuevo Contrato</h2>
+          <form>
+            <input type="text" placeholder="Nombre del cliente" required />
+            <input type="number" placeholder="Monto del contrato" required />
+            <input type="text" placeholder="Propiedad Adquirida" required />
+            <textarea placeholder="Descripción"></textarea>
+            <div className="modal-form-actions">
+              <button type="submit" className="btn btn-success">Guardar</button>
+              <button 
+                type="button" 
+                className="btn btn-secondary"
+                onClick={() => setShowForm(false)}
+              >
+                Cancelar
+              </button>
+            </div>
+          </form>
+        </div>
+      )}
+
+      {/* Botón flotante abajo a la derecha */}
+      <button 
+        className="fab-button" 
+        onClick={() => setShowForm(!showForm)}
+        title="Agregar Propiedad"
+      >
+        +
+      </button>
+
     </main>
   );
 }
