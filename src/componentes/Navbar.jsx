f813889d761a69fc0bda0f81f9e57edcfe77e3d3
Genaro <genarorosti@hotmail.com>
2026-06-10 22:33:10 -0300

    Responsive botones hoome

--- src/componentes/Navbar.jsx
+++ src/componentes/Navbar.jsx
@@ -1,6 +1,14 @@
 import "./navbar.css";
+import { useState } from "react";
+import "./navbar.css";
 
 function Navbar({ darkMode, onToggleDarkMode, onAdminClick }) {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <header className="navbar">
       <div className="container nav-content">
@@ -9,31 +17,40 @@ function Navbar({ darkMode, onToggleDarkMode, onAdminClick }) {
           inmobiliaria
         </div>
 
-        <nav className="nav-links">
+        <button className="hamburger" onClick={toggleMenu}>
+          ☰
+        </button>
+
+        <nav className={`nav-links ${isMenuOpen ? "active" : ""}`}>
           <a href="#inicio">Inicio</a>
           <a href="#propiedades">Propiedades</a>
           <a href="#servicios">Servicios</a>
-        </nav>
+        
 
-        <div className="nav-buttons">
-          <button
-            type="button"
-            className="nav-link-button nav-link-button-theme"
-            onClick={onToggleDarkMode}
-          >
-            {darkMode ? "Modo Claro" : "Modo Oscuro"}
-          </button>
-          <a href="#agendar">
-            <button className="btn-primary">Agendar Cita</button>
-          </a>
-          <button
-            type="button"
-            className="nav-link-button nav-link-button-admin"
-            onClick={onAdminClick}
-          >
-            Modo Admin
-          </button>
-        </div>
+        <button
+    type="button"
+    className="nav-link-button nav-link-button-theme"
+    onClick={onToggleDarkMode}
+  >
+    {darkMode ? "Modo Claro" : "Modo Oscuro"}
+  </button>
+
+  <a href="#agendar">
+    <button className="btn-primary">
+      Agendar Cita
+    </button>
+  </a>
+
+  <button
+    type="button"
+    className="nav-link-button nav-link-button-admin"
+    onClick={onAdminClick}
+  >
+    Modo Admin
+  </button>
+</nav>
+
+ 
       </div>
     </header>
   );
